8e8ea93d4630e30b60c815e2d8dc473ff80c4ec1
89apt89 <89apt89@protonmail.ch>
2019-07-22 00:12:31 -0400

    remove old code

--- punish/punish.py
+++ punish/punish.py
@@ -37,26 +37,4 @@ class PunishCog(Cog):
         await channel.set_permissions(role, send_messages=True)
         await channel.send(f"<@!{user.id}>, you may only talk here now.")
         await ctx.send(f"<@!{user.id}> quarantined successfully.")
-        await ctx.send("https://giphy.com/gifs/ban-banned-admin-fe4dDMD2cAU5RfEaCU")
-
-def find_pings(text):
-    """returns a list of ids"""
-    char1 = "<@!"
-    char12 = "<@"
-    char2 = ">"
-    char1ins = find_substring(char1, text)
-    if len(char1ins) == 0:
-        char1 = "<@"
-        char1ins = find_substring(char12, text)
-    char2ins = find_substring(char2, text)
-    #if len(char1ins) == 0 or len(char2ins) == 0:
-    #    return None
-    ids = []
-    for k, v in enumerate(char1ins):
-        try:
-            char2ins[k]
-        except:
-            break
-    
-        ids.append(text[char1ins[k]+len(char1):char2ins[k]])
-    return ids
+        await ctx.send("https://giphy.com/gifs/ban-banned-admin-fe4dDMD2cAU5RfEaCU"
