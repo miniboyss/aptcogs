4641ae0b8fddc0a4e45c98fe3aae492d6fc50e34
89apt89 <89apt89@protonmail.ch>
2020-02-24 18:14:25 -0500

    markov

--- markov/markov.py
+++ markov/markov.py
@@ -3,12 +3,14 @@ from discord.ext import commands
 from redbot.core import Config, checks, commands
 from discord.utils import get
 from typing import Any
+from markovgen import MarkovGen
 
 Cog: Any = getattr(commands, "Cog", object)
 
 class MarkovCog(Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.markov = MarkovGen(open('text.txt'))
 
     @commands.command(name="markov_test", alias="")
     async def _test(self, ctx):
@@ -19,4 +21,7 @@ class MarkovCog(Cog):
         with open('text.txt', "a+") as f:
             f.write(message.content+"\n")
 
-    
+        if message.startswith("hey minebot"):
+            await message.channel.send(self.markov.generate_markov_text())
+
+        
